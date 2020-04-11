cd62cb17347fe8404d59ad0164bcf89a776ecc54
Pakkapon Phongthawee <me@pureapp.in.th>
2020-04-12 01:04:27 +0700

    support: multi threading

--- deeparc2whitebalance.py
+++ deeparc2whitebalance.py
@@ -1,21 +1,14 @@
 
-import argparse
-import os 
+import argparse, os, cv2
 import numpy as np 
 import matplotlib.pyplot as plt
-import cv2
- 
-def gamma_correction(image,gamma = 1):
-    return np.power(image,1)
-
-def remove_bad_pixel(image):
-    return np.clip(image,0,1)
+from multiprocessing import Pool
 
 def read_exr(image_path):
     image = cv2.imread(image_path,  cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)  
     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    image = remove_bad_pixel(image)
-    image = gamma_correction(image)
+    image = np.clip(image,0,1) # remove bad pixel above 1 
+    image = np.power(image,1) # gamma correction
     return image
 
 def read_mask(mask_path):
@@ -24,48 +17,50 @@ def read_mask(mask_path):
         mask = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
     return mask > 0
 
-def avg_pixel(image,mask):
-    return np.average(image[mask],axis=0)
-
-def normalize_pixel(image,avg_px):
-    image = image / avg_px
-    image = np.clip(image,0,1)
+def normalize_pixel(image,mask):
+    average_pixel = np.average(image[mask],axis=0) 
+    image = image / average_pixel
+    image = np.clip(image,0,1) # image should be in [0,1]
     return image
 
-def get_masks(mask_dir, thread_number = 8):
+def get_masks(mask_dir, num_thread = 8):
     mask_files = os.listdir(mask_dir)
+    mask_paths = []
     masks = {}
+    mask_name_list = []
     for mask_file in mask_files:
         if not mask_file.endswith('.png'):
             continue
-        mask_name = mask_file.split('.')[0]
-        mask_pixel = read_mask(os.path.join(mask_dir,mask_file))
-        masks[mask_name] = mask_pixel
+        mask_paths.append(os.path.join(mask_dir,mask_file))
+        mask_name_list.append(mask_file.split('.')[0])
+    with Pool(num_thread) as p:
+        mask_list = p.map(read_mask,mask_paths)
+    for i in range(len(mask_list)):
+        masks[mask_name_list[i]] = mask_list[i]
     return masks 
 
+def apply_mask(packed_data):
+    image = read_exr(packed_data['image_path'])
+    image = normalize_pixel(image,packed_data['mask'])
+    plt.imsave(packed_data['output_path'],image)
 
-def apply_masks(input_dir, output_dir, masks):
+def apply_masks(input_dir, output_dir, masks, num_thread = 8):
+    packed_datas = []
     exr_files = os.listdir(input_dir)
     for exr_file in exr_files:
         if not exr_file.endswith('.exr'):
-            continue
-        mask = masks[exr_file.split('_')[0]]
-        image = read_exr(os.path.join(input_dir,exr_file))
-        average_pixel = avg_pixel(image,mask)
-        image = normalize_pixel(image,average_pixel)
-        file_name = exr_file.split('.')[0]
-        plt.imsave(os.path.join(output_dir,file_name + '.png'),image)
+             continue
+        packed_datas.append({
+            'image_path': os.path.join(input_dir,exr_file),
+            'mask': masks[exr_file.split('_')[0]],
+            'output_path': os.path.join(output_dir, exr_file.split('.')[0] + '.png')
+        })
+    with Pool(num_thread) as p:
+        p.map(apply_mask,packed_datas)
 
 def main(args):
     masks = get_masks(args.reference, args.thread)
-    apply_masks(args.input, args.output, masks)
-
-    #image = read_exr(os.path.join(args.input,'cam000_00000.exr'))
-    #mask = read_mask(os.path.join(args.reference,'cam000_00000.png'))
-    #average_pixel = avg_pixel(image,mask)
-    #image = normalize_pixel(image,average_pixel)
-    #plt.imshow(image)
-    #plt.show()
+    apply_masks(args.input, args.output, masks, args.thread)
  
 def entry_point():
     parser = argparse.ArgumentParser(
